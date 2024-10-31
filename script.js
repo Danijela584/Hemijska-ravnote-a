410ca8d00f594627c88ad255aa1cd0c6d283afa4
Danijela584 <145586012+Danijela584@users.noreply.github.com>
2024-10-31 20:59:42 +0100

    script.js

--- script.js
+++ script.js
@@ -11,12 +11,26 @@ const reactions = [
     { equation: "C (s) + CO2 (g) ⇄ CO (g)", correctSide: "right" }
 ];
 
+const questions = [
+    "Kako se menja ravnoteža povećanjem koncentracije ",
+    "Kako se menja ravnoteža smanjenjem koncentracije ",
+    "Kako se menja ravnoteža povećanjem pritiska?",
+    "Kako se menja ravnoteža smanjenjem zapremine?",
+    "Kako se menja ravnoteža povećanjem zapremine?",
+    "Kako se menja ravnoteža povećanjem temperature?",
+    "Kako se menja ravnoteža smanjenjem temperature?"
+];
+
 let currentReaction = 0;
+let currentQuestion = 0;
 
 document.getElementById('reaction').innerText = reactions[currentReaction].equation;
+document.getElementById('question').innerText = questions[currentQuestion] + reactions[currentReaction].equation;
 
 document.getElementById('left-button').addEventListener('click', () => checkAnswer('left'));
 document.getElementById('right-button').addEventListener('click', () => checkAnswer('right'));
+document.getElementById('yes-button').addEventListener('click', () => checkQuestion('yes'));
+document.getElementById('no-button').addEventListener('click', () => checkQuestion('no'));
 
 function checkAnswer(side) {
     const resultElement = document.getElementById('result');
@@ -29,4 +43,20 @@ function checkAnswer(side) {
     }
     currentReaction = (currentReaction + 1) % reactions.length;
     document.getElementById('reaction').innerText = reactions[currentReaction].equation;
+    document.getElementById('question').innerText = questions[currentQuestion] + reactions[currentReaction].equation;
+}
+
+function checkQuestion(answer) {
+    const answerElement = document.getElementById('answer');
+    // Logic to determine the correct answer based on Le Chatelier's principle
+    // This is a simplified example and should be expanded based on specific rules
+    if (answer === 'yes') {
+        answerElement.innerText = "Correct!";
+        answerElement.className = "correct";
+    } else {
+        answerElement.innerText = "Incorrect!";
+        answerElement.className = "incorrect";
+    }
+    currentQuestion = (currentQuestion + 1) % questions.length;
+    document.getElementById('question').innerText = questions[currentQuestion] + reactions[currentReaction].equation;
 }
